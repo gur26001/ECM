c01df08c7334fbb6ca1feb8b12d38e189d7eb562
Gursimran Singh <gurisignh262001@gmail.com>
2022-02-16 19:51:22 +0530

    left eye and right eye extraction done

--- blinkDetector.py
+++ blinkDetector.py
@@ -1,58 +1,35 @@
 import cv2
+# import numpy as np
 import faceMesh as FM
-import time
+
+
+
+detector= FM.FaceDetector(maxFaces=1,refinedDetection=True)
 cam = cv2.VideoCapture(0)
-cam.set(3,640)
-cam.set(4,480)
 
-detector = FM.FaceDetector(refinedDetection=True)
-
-#upper outline, upper innerline, bottom innerline
-LEFT_EYE = [257,[398,384,385,386,387,388,466],[382,281,249,390,373,374,280]]
-rightList=[27,[246,161,160,159,158,157,173],[55,54,53,45,44,63,7]]
-# rightList = [33,7,163,144,145,153,154,155,133,173,157,158,159,160,161,246]
-count=0
-
-
-
-def initBlinkState():
-    for i in range(0,2):
-        status, img = cam.read()
-        imgRGB= cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        detector.Process(imgRGB)
-        rightUp = detector.getLandMarkOf(0,159)
-        rightDown = detector.getLandMarkOf(0,145)
-
-    return rightUp[1],rightDown[1]
-
-initialRightUppery,initialRightDowny = initBlinkState()
-
-blinkCounter=0
-currRightUppery, currRightDowny = detector.getLandMarkOf(0,159)[1],detector.getLandMarkOf(0,23)[1]
-while cam.isOpened():
-    status,img = cam.read()
+while True:
+    st,img= cam.read()
     # img= cv2.flip(img,1)
-    imgRGB= cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     detector.Process(imgRGB)
 
+    leftEyeUpr, leftEyeDown = detector.getLandMarkOf(0, 443), detector.getLandMarkOf(0, 450)
+    leftEyeright, leftEyeRight = detector.getLandMarkOf(0,464), detector.getLandMarkOf(0,359)
 
-    cv2.circle(img,detector.getLandMarkOf(0,386),1,(0,255,0),-1)
-    cv2.circle(img, detector.getLandMarkOf(0,374), 1, (0, 255, 255), -1)
-    # cv2.circle(img,detector.getLandMarkOf(0,159),1,(0,255,0),-1)
-    # cv2.circle(img, detector.getLandMarkOf(0, 145), 1, (0, 255, 255), -1)
+    leftEyeHeight = (leftEyeUpr[1], leftEyeDown[1])
+    leftEyeWidth = (leftEyeright[0], leftEyeRight[0])
+    leftEyeCrop = img[leftEyeHeight[0]:leftEyeHeight[1], leftEyeWidth[0]:leftEyeWidth[1]]
+    leftEyeCrop = cv2.resize(leftEyeCrop,(leftEyeWidth[1] - leftEyeWidth[0],leftEyeHeight[1] - leftEyeHeight[0]))
 
-    prevRightUppery, prevRightDowny = currRightUppery, currRightDowny
-    currRightUppery, currRightDowny = detector.getLandMarkOf(0,159)[1],detector.getLandMarkOf(0,145)[1]
-    # cv2.line(img,rightUp,rightDown,(0,255,0),2)
-    # cv2.line(img, rightLeft, rightRight, (0, 255, 0), 2)
+    rightEyeUpr,rightEyeDown = detector.getLandMarkOf(0,223),detector.getLandMarkOf(0,230)
+    rightEyeright,rightEyeRight=detector.getLandMarkOf(0,130),detector.getLandMarkOf(0,244)
+    rightEyeHeight = (rightEyeUpr[1],rightEyeDown[1])
+    rightEyeWidth =  (rightEyeright[0],rightEyeRight[0])
+    rightEyeCrop= img[rightEyeHeight[0]:rightEyeHeight[1],rightEyeWidth[0]:rightEyeWidth[1]]
+    rightEyeCrop = cv2.resize(rightEyeCrop,(rightEyeWidth[1]-rightEyeWidth[0],rightEyeHeight[1]-rightEyeHeight[0]))
 
-    if((currRightUppery>prevRightUppery and currRightDowny<prevRightDowny)):
-        blinkCounter+=1
-        print("Blinked")
-
-    cv2.imshow("img",img)
-
-    if (cv2.waitKey(1)==ord('q')):
+    cv2.imshow("leftEyeCrop",leftEyeCrop)
+    cv2.imshow("rightEyeCrop",rightEyeCrop)
+    cv2.imshow("orignal",img)
+    if cv2.waitKey(1)==ord('q'):
         break
-
-print(blinkCounter)
