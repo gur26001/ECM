65803143c62aba1eed3be04789e216f4a9649f41
Gursimran Singh <gurisignh262001@gmail.com>
2022-02-20 01:32:13 +0530

    reference coordinate found (mid of nose) for detection of angle to find the distance to move to right or left

--- eyesMotionDetector.py
+++ eyesMotionDetector.py
@@ -2,15 +2,19 @@ import cv2
 import commonfile as cf
 import faceMesh as FM
 import blinkDetector as bd
+import irisdetection as irisd
 
 cam = cv2.VideoCapture(0)
 cam.set(3,1280)
 cam.set(4,890)
 LEFT_IRIS = [474,475,476,477]
 RIGHT_IRIS = [469,470,471,472]
+MID_NOSE  = [163,6,173,398]
 detector = FM.FaceDetector(refinedDetection=True)
 
+
 def initialState():
+    val=0
     for i in range(0,5):
         st, img = cam.read()
         detector.Process(img)
@@ -29,20 +33,35 @@ def initialState():
         leftEyeShadowDown = detector.getLandMarkOf(0, 386)
         leftEyeShadowDistBtw, _ = cf.findDistance(leftEyeShadowUpper, leftEyeShadowDown)
 
+
         rightEyeShadowUpper = detector.getLandMarkOf(0, 27)
         rightEyeShadowDown = detector.getLandMarkOf(0, 159)
         rightEyeShadowDistBtw, _ = cf.findDistance(leftEyeShadowUpper, leftEyeShadowDown)
 
-        cv2.imshow("img", img)
-        if (cv2.waitKey(1) == ord('q')):
-            break
-    return [(leftEyeDis,rightEyeDis),(leftEyeShadowDistBtw,rightEyeShadowDistBtw)]
 
+        val=[(leftEyeDis,rightEyeDis),(leftEyeShadowDistBtw,rightEyeShadowDistBtw)]
+
+    return val
+
+def moveLeft():
+    pass
+def moveRight():
+    pass
+def moveUp():
+    pass
+def moveDown():
+    pass
 
 initVals= initialState()
 
+irisdetector = irisd.IrisDetector()  #coordinates(left,right) , radius(left,right)
+
+# faceMid = [168,6,]
+
+
 while True:
     st,img = cam.read()
+
 
     #left -> distance b/w eye corner point in direction of the eye outside corner
 
@@ -58,37 +77,62 @@ while True:
     # cv2.putText(img, f'{33}', righteye, cv2.FONT_HERSHEY_PLAIN, 0.5, (255, 255, 0))
     # right -> distance b/w eye corner point in direction of the eye outside corner
 
-    #up -> eyeshadow dist dec only and no need to cal. dist b/w uper lash and lower lash
 
+##################detection of the mid of the nose to get the reference line for left and right angle
+
+    centerofnose= detector.getLandMarkOf(0,168)
+    cv2.circle(img,centerofnose,2,(0,0,255),-1)
+########################################################################################
 
     #down->blinkdetector use
     leftIrisCorner = detector.getLandMarkOf(0,474 ) #jisto use krke dist find krna
     leftEyeCorner = detector.getLandMarkOf(0,263)
     leftEyeDis,_ = cf.findDistance(leftIrisCorner,leftEyeCorner)
-    print(leftEyeDis,end=' ')
+    # print(leftEyeDis,end=' ')
 
     rightIrisCorner = detector.getLandMarkOf(0,471)
     rightEyeCorner = detector.getLandMarkOf(0,33)
     rightEyeDis,_ = cf.findDistance(rightEyeCorner,rightIrisCorner)
-    print(rightEyeDis)
+    # print(rightEyeDis)
 
     leftEyeShadowUpper = detector.getLandMarkOf(0, 257)
     leftEyeShadowDown = detector.getLandMarkOf(0, 386)
+
+
     leftEyeShadowDistBtw,_ = cf.findDistance(leftEyeShadowUpper,leftEyeShadowDown)
+
+
 
 
     rightEyeShadowUpper = detector.getLandMarkOf(0, 27)
     rightEyeShadowDown = detector.getLandMarkOf(0, 159)
     rightEyeShadowDistBtw,_ = cf.findDistance(leftEyeShadowUpper,leftEyeShadowDown)
 
+    resiris = irisdetector.Process(img)
 
+
+    # rightIrisDis = format((cf.findDistance(resiris[1],valright)[0]),".2f")
+    # cv2.putText(img,f'dist{rightIrisDis}',(resiris[1][0]-10,resiris[1][1]-5),cv2.FONT_HERSHEY_PLAIN,1,(0,255,0),1)
+    #
+    #
+    cv2.line(img,resiris[1],centerofnose,(0,255,0))
+    #
+    #
+    # leftIrisDis = format((cf.findDistance(resiris[0],valleft)[0]),".2f")
+    # cv2.putText(img,f'dist{leftIrisDis}',(resiris[0][0] - 10, resiris[0][1] - 5), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+    #
+    cv2.line(img,resiris[0],centerofnose,(0,255,0))
+
+    # cv2.putText(img, f'dist{cf.findDistance(detector.getLandMarkOf(0, 469), detector.getLandMarkOf(0, 133))[0]}',(resiris[1][0] - 10, resiris[1][1] - 10), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+    # cv2.line(img, detector.getLandMarkOf(0, 469), detector.getLandMarkOf(0, 133), (0, 255, 0))
 
     if(initVals[0][0]>leftEyeDis): #left
-        cv2.putText(img,"LEFT",(100,100),cv2.FONT_HERSHEY_PLAIN,2,(255,0,255))
+        cv2.putText(img,f"LEFT{initVals[0][0]-leftEyeDis}",(100,100),cv2.FONT_HERSHEY_PLAIN,2,(255,0,255))
+
     else:
         pass
     if(initVals[0][1]>rightEyeDis): #right
-        cv2.putText(img,"RIGHT",(100,200),cv2.FONT_HERSHEY_PLAIN,2,(255,0,255))
+        cv2.putText(img,f"RIGHT{initVals[0][1]-rightEyeDis}",(100,200),cv2.FONT_HERSHEY_PLAIN,2,(255,0,255))
     else:
         pass
     if(initVals[1][0]>leftEyeShadowDistBtw and initVals[1][1]>rightEyeShadowDistBtw):#up            one issue if person is shocked then it will detect as up
